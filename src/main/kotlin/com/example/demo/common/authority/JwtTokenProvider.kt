5d7fada3129c043d90be00c15521664d593beafc
Yohwan Lim <dyghks7102@naver.com>
2024-04-03 23:14:23 +0900

    feat: 내 정보 조회기능 v2

--- src/main/kotlin/com/example/demo/common/authority/JwtTokenProvider.kt
+++ src/main/kotlin/com/example/demo/common/authority/JwtTokenProvider.kt
@@ -1,5 +1,6 @@
 package com.example.demo.common.authority
 
+import com.example.demo.common.dto.CustomUser
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
@@ -39,6 +40,7 @@ class JwtTokenProvider {
         val accessToken = Jwts.builder()
             .setSubject(authentication.name)
             .claim("auth", authorities)
+            .claim("userId", (authentication.principal as CustomUser).userId)
             .setIssuedAt(now)
             .setExpiration(accessExpiration)
             .signWith(key, SignatureAlgorithm.HS256)
@@ -51,12 +53,13 @@ class JwtTokenProvider {
         val claims: Claims = getClaims(token)
 
         val auth = claims["auth"] ?: throw IllegalStateException("잘못된 토큰입니다.")
+        val userId = claims["userId"] ?: throw IllegalStateException("잘못된 토큰입니다.")
 
         val authorities: Collection<GrantedAuthority> = (auth as String)
             .split(",")
             .map { SimpleGrantedAuthority(it) }
 
-        val principal: UserDetails = User(claims.subject, "", authorities)
+        val principal: UserDetails = CustomUser(userId.toString().toLong(), claims.subject, "", authorities)
 
         return UsernamePasswordAuthenticationToken(principal, "", authorities)
     }
